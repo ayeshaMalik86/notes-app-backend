6c7d1d688e99308f5616991fbcf09ff926125ed4
ayeshaMalik86 <ayesomestyles@gmail.com>
2025-10-08 11:53:36 +0500

    feat: add CORS configuration for added security in production

--- server.js
+++ server.js
@@ -8,7 +8,13 @@ import authRoutes from './routes/authRoutes.js';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: [
+        process.env.CLIENT_URL_LOCAL,
+        process.env.CLIENT_URL_PROD,
+    ].filter(Boolean),
+    credentials: true,
+}));
 app.use(express.json());
 
 connectDB();
